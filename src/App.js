11a7990e9eb9d9122be62602a4852009a274d38b
vvatom <112102207+vvatom@users.noreply.github.com>
2023-06-23 02:16:17 +0200

    Create bacground for portfolio

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hello i'm vvatom
-      </header>
+      <div className='circle'>
+      <div className='circle__eclipse'></div>
+      <div className='circle__light'></div>
+      <span className='circle__blue--anim'></span>
+      <span className='circle__blue--anim circle__blue--blur_1'></span>
+      <span className='circle__blue--anim circle__blue--blur_2'></span>
+      <span className='circle__blue--anim circle__blue--blur_3'></span>
+      <span className='circle__blue--anim circle__blue--blur_4'></span>
+      <span className='circle__blue--anim circle__blue--white_ray'></span>
+
+      <span className='circle__purple--anim'></span>
+      <span className='circle__purple--anim circle__purple--blur_1'></span>
+      <span className='circle__purple--anim circle__purple--blur_2'></span>
+      <span className='circle__purple--anim circle__purple--blur_3'></span>
+      <span className='circle__purple--anim circle__purple--blur_4'></span>
+      <span className='circle__purple--anim circle__purple--white_ray'></span>
+      </div>
+      <div className='circle__photo'></div>
     </div>
   );
 }
