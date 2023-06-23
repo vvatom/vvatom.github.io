fb2dc1e7d3fba5fd010f2debea88f5a4df006dff
vvatom <112102207+vvatom@users.noreply.github.com>
2023-06-23 14:44:22 +0200

    Create triangle

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='front'>
+
       <div className='circle'>
       <div className='circle__eclipse'></div>
       <div className='circle__light'></div>
+
+      <div className='triangle_left'>
+        <span className='triangle_left--anim'></span>
+        <span className='triangle_left--anim_reverse'></span>
+      </div>
+      <div className='triangle_right'>
+        <span className='triangle_right--anim'></span>
+        <span className='triangle_right--anim_reverse'></span>
+      </div>
+
       <span className='circle__blue--anim'></span>
       <span className='circle__blue--anim circle__blue--blur_1'></span>
       <span className='circle__blue--anim circle__blue--blur_2'></span>
@@ -21,6 +33,11 @@ function App() {
       <span className='circle__purple--anim circle__purple--white_ray'></span>
       </div>
       <div className='circle__photo'></div>
+      <div className='triangle_bottom'>
+        <span className='triangle_bottom--anim'></span>
+        <span className='triangle_bottom--anim_reverse'></span>
+      </div>
+      </div>
     </div>
   );
 }
